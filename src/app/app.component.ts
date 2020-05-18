ce86ecab78f145bbfe3fa7faf9ea164c1f7b8314
Aktilek <tohakktl@gmail.com>
2020-05-18 19:41:48 +0600

    refa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  host: {
-    "(window:resize)":"onWindowResize($event)"
-  }
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isMobile: boolean = false;
-  width:number = window.innerWidth;
-  height:number = window.innerHeight;
-  mobileWidth:number  = 760;
-
   constructor() {
 
   }
 
   ngOnInit(): void {
-    this.isMobile = this.width < this.mobileWidth;
-  }
-  onWindowResize(event) {
-    this.width = event.target.innerWidth;
-    this.height = event.target.innerHeight;
-    this.isMobile = this.width < this.mobileWidth;
   }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,32 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css'],
-  host: {
-    "(window:resize)":"onWindowResize($event)"
-  }
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  isMobile: boolean = false;
-  width:number = window.innerWidth;
-  height:number = window.innerHeight;
-  mobileWidth:number  = 760;
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-    this.isMobile = this.width < this.mobileWidth;
-
-  }
-  onWindowResize(event) {
-    this.width = event.target.innerWidth;
-    this.height = event.target.innerHeight;
-    this.isMobile = this.width < this.mobileWidth;
   }
   scroll(id) {
     console.log(`scrolling to ${id}`);
